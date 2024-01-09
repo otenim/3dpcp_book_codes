b9ebdb817d6a506328aba3ce2e9991231d827625
otenim <snake_istobelieve@keio.jp>
2024-01-09 22:55:14 +0900

    update

--- section_basics/o3d_visualize_points.py
+++ section_basics/o3d_visualize_points.py
@@ -5,6 +5,9 @@ import open3d as o3d
 filename = sys.argv[1]
 
 print("Loading a point cloud from", filename)
+# Load as mesh data
+# pcd = o3d.io.read_triangle_mesh(filename)
+# Load as point cloud
 pcd = o3d.io.read_point_cloud(filename)
 
 print(pcd)
